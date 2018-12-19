67eff8e9819d856ff72deda2bc5ebc1e72e9eff6
Dmytro Kharchenko <30443685+dkharch@users.noreply.github.com>
2018-12-20 01:34:47 +0200

    Updates

---
+++ precache-manifest.e27c368d067380d2ce7b95bba65b8fb9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fbed04333d9d9e00346f",
+    "url": "/react-redux-estats/static/js/runtime~main.fbed0433.js"
+  },
+  {
+    "revision": "ca64a2472963022c365f",
+    "url": "/react-redux-estats/static/js/main.ca64a247.chunk.js"
+  },
+  {
+    "revision": "d9a4df0a7da890757354",
+    "url": "/react-redux-estats/static/js/1.d9a4df0a.chunk.js"
+  },
+  {
+    "revision": "ca64a2472963022c365f",
+    "url": "/react-redux-estats/static/css/main.fc30fdef.chunk.css"
+  },
+  {
+    "revision": "c218c92ea8e4e471b8dab398b63479bb",
+    "url": "/react-redux-estats/index.html"
+  }
+];
